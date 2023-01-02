9588c431e4b89275ce282d74061e3487fe93fe93
Jacqueline M <75797321+rookiebehaviour@users.noreply.github.com>
2023-01-02 17:58:45 -0500

    Initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,15 @@
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<form method="POST" action="?/login">
+  <label>
+    Email
+    <input name="email" type="email" autocomplete="off">
+  </label>
+  <label>
+    Password
+    <input name="password" type="password">
+  </label>
+  <button>Log in</button>
+	<button formaction="?/register">Register</button>
+</form>

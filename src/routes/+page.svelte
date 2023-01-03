36f1281845eed48ccc377b830dd00fba3795fa89
Jacqueline M <75797321+rookiebehaviour@users.noreply.github.com>
2023-01-03 14:50:48 -0500

    Edited heading and set name to empty string

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,21 +1,24 @@
 <h1>Hello, I'm Jacqueline 👋🏽</h1>
 <h2>Feel free to play around with my Svelte app</h2>
-<h3>I'm still learning Svelte so let me know if it's buggy</h3>
+<h3>I'm still learning Svelte so this is my playground 👩🏽‍💻</h3>
 
 <script>
 	let a = 1;
 	let b = 2;
-  let name = "Jacqueline";
+  let name = "";
+
 </script>
 
 <form>
-  <input label="Name" type="string" bind:value={name}>
+  <input placeholder="Enter your name" type="string" bind:value={name}>
   <button>Save</button>
 <input type="number" bind:value={a}>
 <input type="number" bind:value={b}>
 </form>
 
 <p>{a} + {b} = {a + b} 
+  <br/>
+<br/>
   Hello {name}</p>
 
 <style>
@@ -35,5 +38,18 @@
     border: 2px solid #870000;
   }
 
+  button {
+    padding:10px 15px;
+    border-radius: 8px;
+    background: #fff;
+    border: none;
+    box-shadow: 2px 2px 3px black;
+  }
+
+  button:hover {
+    background: #870000;
+    color: #fff;
+    
+  }
 	
 </style>
